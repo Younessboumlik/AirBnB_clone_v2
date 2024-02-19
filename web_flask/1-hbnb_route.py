ff53d1c5dd9eae35dee0feab28ab82073d72e83a
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2024-02-19 14:23:09 +0100

    task1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,16 +4,19 @@ starts a Flask web application
 """
 
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route("/",strict_slashes=False)
-def main():
-  """returns Hello HBNB!"""
-  return "Hello HBNB!"
-@app.route("/hbnb",strict_slashes=False)
-def main2():
-  """returns HBNB"""
-  return "HBNB"
 
-app.run(port = 5000,host="0.0.0.0")
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns HBNB"""
+    return 'HBNB'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
