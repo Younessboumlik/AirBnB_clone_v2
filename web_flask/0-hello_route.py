4493d47f1bb6d1659b35513d73fe8bf78c24988f
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2024-02-19 19:13:50 +0100

    task0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,15 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/",strict_slashes=False)
 def main():
+  """return hello hbnb"""
   return "Hello HBNB!"
-
-app.run(port = 5000,host="0.0.0.0")
+if __name__ = "__main__":
+  app.run(port = 5000,host="0.0.0.0")
